84ed00c0d0f4e999ec24b0606a1ba3dad443a0cd
josepereza <josepereza66@gmail.com>
2021-10-25 17:39:33 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, animateChild, group, query, stagger, state, style, transition, trigger } from '@angular/animations';
+import { animate, animateChild, group, keyframes, query, stagger, state, style, transition, trigger } from '@angular/animations';
 import { Component, VERSION,OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import {HEROES} from './data'
@@ -33,9 +33,30 @@ var tiempo=300
          transform:'translateX(0)',opacity:1
        }))
       ])
-    ])
+    ]),
+   
+    trigger('segundaAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(':enter', stagger('300ms', [
+          animate('.5s ease-in', keyframes([
+            style({ opacity: 0, transform: 'translateX(-50%)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateX(-10px) scale(1.1)', offset: 0.3 }),
+            style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
+          ]))]), { optional: true }),
+        query(':leave', stagger('300ms', [
+          animate('1s ease-out', keyframes([
+            style({ opacity: 1, transform: 'scale(1.1)', offset: 0 }),
+            style({ opacity: .5, transform: 'scale(.5)', offset: 0.3 }),
+            style({ opacity: 0, transform: 'scale(0)', offset: 1 }),
+          ]))]), { optional: true })
+      ]),
+    ]),
+      
   ]
 })
+
+
 export class AppComponent implements OnInit  {
   misheroes=of(HEROES)
   heroes:any[]=[]
